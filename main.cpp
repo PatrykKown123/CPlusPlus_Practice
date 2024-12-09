b822849a6127bdfe28ec9f7d1a65cca69ecf791a
pattycake1337 <159066586+pattycake1337@users.noreply.github.com>
2024-12-09 13:45:38 -0500

    Having issues with string input - buffer is accepting new line.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 #include "Logic/Car.h"
 int main() {
     Logic::Car* carDB[10];
+    int carCount = 0;
     while (true) {
         std::cout << "Welcome to car implementation!" << std::endl;
+
         std::cout << "Are you adding either an ICE or Electric Car?" << std::endl;
         std::string carType;
         std::cin >> carType;
         if(carType != "ICE" || carType != "Electric Car") {
             std::cout << "Invalid input, please enter an ICE or Electric Car" << std::endl;
-        } else if(carType == "ICE") {
+        }
+        if(carType == "ICE") {
             std::cout << "You are creating a: "
             << carType
             << " car." << std::endl
@@ -20,9 +23,11 @@ int main() {
             std::cout << "Please enter your license plate number: ";
             std::string plateNumber;
             std::cin >> plateNumber;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Please enter your car make: ";
             std::string carMake;
             std::cin >> carMake;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "Please enter your effiency (1 or 0): " << std::endl;
             float efficiency;
             std::cin >> efficiency;
@@ -43,11 +48,47 @@ int main() {
             } catch(std::exception& e) {
                 std::cout << e.what()<< std::endl;
             }
+            if(carCount < 10) {
+                carDB[carCount++] = new Logic::ICECar(energy, capacity, plateNumber, carMake, efficiency);
+            }
+        } else if(carType == "Electric Car") {
+            std::cout << "You are creating a: "
+            << carType
+            << " car." << std::endl
+            << std::endl;
 
+            std::cout << "Please enter your license plate number: ";
+            std::string plateNumber;
+            std::cin >> plateNumber;
 
+            std::cout << "Please enter your car make: ";
+            std::string carMake;
+            std::cin >> carMake;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Please enter your effiency (1 or 0): " << std::endl;
+            float efficiency;
+            std::cin >> efficiency;
+            if(efficiency != 0 || efficiency != 1) {
+                std::cout << "Invalid input, please enter 1 or 0" << std::endl;
+            }
+            float energy;
+            float capacity;
+            try {
+                std::cout << "Please enter your fuel consumption per KM: " << std::endl;
+                std::cin >> energy;
+            } catch(std::exception& e) {
+                std::cout << e.what()<< std::endl;
+            }
+            try {
+                std::cout << "Please enter your tank capacity in liters " << std::endl;
+                std::cin >> capacity;
+            } catch(std::exception& e) {
+                std::cout << e.what()<< std::endl;
+            }
+            if(carCount < 10) {
+                carDB[carCount++] = new Logic::ElectricCar(energy, capacity, plateNumber, carMake, efficiency);
+            }
         }
-        std::cout << "Welcome to car implementation!" << std::endl;
-        std::cout << "Welcome to car implementation!" << std::endl;
 
     }
     return 0;
