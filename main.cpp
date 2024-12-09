8d9fb39148afedd32ed531446c28af357885378b
pattycake1337 <159066586+pattycake1337@users.noreply.github.com>
2024-12-09 14:24:10 -0500

    Debugging program

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ int main() {
     while (true) {
         std::cout << "Welcome to car implementation!" << std::endl;
 
-        std::cout << "Are you adding either an ICE or Electric Car?" << std::endl;
+        std::cout << "Are you adding either an ICE or Electric?" << std::endl;
         std::string carType;
         std::cin >> carType;
-        if(carType != "ICE" || carType != "Electric Car") {
-            std::cout << "Invalid input, please enter an ICE or Electric Car" << std::endl;
-        }
+        std::cin.ignore(25, '\n');
+        // if(carType != "ICE" || carType != "Electric") {
+        //     std::cout << "Invalid input, please enter an ICE or Electric" << std::endl;
+        // }
         if(carType == "ICE") {
             std::cout << "You are creating a: "
             << carType
@@ -23,14 +24,15 @@ int main() {
             std::cout << "Please enter your license plate number: ";
             std::string plateNumber;
             std::cin >> plateNumber;
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.ignore(25, '\n');
             std::cout << "Please enter your car make: ";
             std::string carMake;
             std::cin >> carMake;
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.ignore(25, '\n');
             std::cout << "Please enter your effiency (1 or 0): " << std::endl;
             float efficiency;
             std::cin >> efficiency;
+            std::cin.ignore(25, '\n');
             if(efficiency != 0 || efficiency != 1) {
                 std::cout << "Invalid input, please enter 1 or 0" << std::endl;
             }
@@ -39,19 +41,21 @@ int main() {
             try {
                 std::cout << "Please enter your energy consumption per KM: " << std::endl;
                 std::cin >> energy;
+                std::cin.ignore(25, '\n');
             } catch(std::exception& e) {
                 std::cout << e.what()<< std::endl;
             }
             try {
                 std::cout << "Please enter your battery capacity in kWh " << std::endl;
                 std::cin >> capacity;
+                std::cin.ignore(25, '\n');
             } catch(std::exception& e) {
                 std::cout << e.what()<< std::endl;
             }
             if(carCount < 10) {
                 carDB[carCount++] = new Logic::ICECar(energy, capacity, plateNumber, carMake, efficiency);
             }
-        } else if(carType == "Electric Car") {
+        } else if(carType == "Electric") {
             std::cout << "You are creating a: "
             << carType
             << " car." << std::endl
